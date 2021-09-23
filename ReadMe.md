0a484134af713e22ef03084722947ff45bf6f65a
NseckinM <seckinmantar@gmail.com>
2021-09-23 12:09:33 +0300

    IAsyncRepository

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# WatchShop
+A sample ASP.NET Project With Clean Architecture based on 
+https://github.com/dotnet-architecture/eShopOnWeb
+
+## Web
+
+## Infrastructure
+
+## ApplicationCore
+Install-Package Ardalis.Specification
---
+++ src/ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationCore.Etities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore
+{
+    //T kullanarak generic yaptık ama baseentity den miras alanların türünde olabilir diye kısıtladık
+    //example IAsyncRepository<Product> gibi
+    public interface IAsyncRepository<T> where T : BaseEntity  
+    {
+
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> ListAllAsync();
+
+        Task<T> ListAsync(ISpecification<T> spec);
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+
+        Task CountAsync(ISpecification<T> spec);
+        Task FirstAsync(ISpecification<T> spec);
+        Task FirstOrDefaultAsync(ISpecification<T> spec);
+
+
+
+
+    }
+}
